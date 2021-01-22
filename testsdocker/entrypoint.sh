7d7a25f33e9f81096b15b342ea0b0fe9571908e8
eric.petroelje <eric.petroelje@cqlcorp.com>
2021-01-22 16:17:24 -0500

    Try this with docker.

---
+++ testsdocker/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Exit if any subcommand fails
+set -e
+cd $GITHUB_WORKSPACE
+
+if [ -f "Gemfile" ]; then
+  echo "# Bundling..."
+  bundle install --jobs 4 --retry 3
+fi
+
+bundle exec rspec
