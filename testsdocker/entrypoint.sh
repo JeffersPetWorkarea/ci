7cdedddfda9b7bedc633f7292ab1a2799190c552
Eric Petroelje <eric.petroelje@cqlcorp.com>
2021-01-25 12:09:09 -0500

    Update entrypoint.sh

--- testsdocker/entrypoint.sh
+++ testsdocker/entrypoint.sh
@@ -6,8 +6,10 @@ cd $GITHUB_WORKSPACE
 
 if [ -f "Gemfile" ]; then
   echo "# Bundling..."
+  gem install bundler -v 1.17.3
   bundle install --deployment --jobs 4 --retry 3
 fi
 
 export RAILS_ENV=test
+
 rake ci:setup:rspec spec --trace || true
