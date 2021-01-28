c8d2a224e416b76282d63749e501bb2bde73700d
Eric Petroelje <eric.petroelje@cqlcorp.com>
2021-01-28 14:12:29 -0500

    Update entrypoint.sh

--- testsdocker/entrypoint.sh
+++ testsdocker/entrypoint.sh
@@ -14,7 +14,7 @@ export RAILS_ENV=test
 export TZ="/usr/share/zoneinfo/America/New_York"
 echo $ELASTICSEARCH_URL
 curl -XGET 'elasticsearch:9200'
-bundle exec rails runner "require 'yaml' && YAML.dump(Rails.application.secrets)"
-bundle exec rails runner "require 'yaml' && YAML.dump(Rails.application.config)"
+#bundle exec rails runner "require 'yaml' && YAML.dump(Rails.application.secrets)"
+#bundle exec rails runner "require 'yaml' && YAML.dump(Rails.application.config)"
 #bundle exec rspec ./spec/controllers/admin/blog_entries_controller_spec.rb
 bundle exec rake ci:setup:rspec spec --trace
